8315888c859ff466a1b43b486c79d403c9eaff04
Sampad <bebartasampad@gmail.com>
2021-01-21 16:04:23 +0530

    Program to check 2 lines equal or not

--- LineComputation.java
+++ LineComputation.java
@@ -1,13 +1,29 @@
 public class LineComputation {
+
+	float length;
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to LINE COMPARISON COMPUTATION PROGRAM.");
+		System.out.println("Welcome to LINE COMPARISON COMPUTATION PROGRAM.\n");
 
 		// VARIABLES
-		double x1 = Math.random();
-		double y1 = Math.random();
-		double x2 = Math.random();
-		double y2 = Math.random();
-		float lineLength = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-		System.out.println("Line length is " + lineLength + " units.");
+		double x1, y1, x2, y2;
+
+		LineComputation line1 = new LineComputation();
+		LineComputation line2 = new LineComputation();
+
+		// LINE LENGTHS EQUALITY CHECK
+		for (int count = 1; count <= 2; count++) {
+			x1 = Math.random();
+			y1 = Math.random();
+			x2 = Math.random();
+			y2 = Math.random();
+			if (count == 1)
+				line1.length = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+			else
+				line2.length = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		}
+		System.out.println("Line1 length is " + line1.length + " units.");
+		System.out.println("Line1 length is " + line2.length + " units.");
+		System.out.println("The two lines are of equal length. True or false?  = " + line1.equals(line2));
 	}
 }
