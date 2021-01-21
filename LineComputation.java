7161e4e326c306db3d9a82c49ccba2af00820e82
Sampad <bebartasampad@gmail.com>
2021-01-21 15:55:13 +0530

    Program of Line Computation Problem

---
+++ LineComputation.java
@@ -0,0 +1,13 @@
+public class LineComputation {
+	public static void main(String[] args) {
+		System.out.println("Welcome to LINE COMPARISON COMPUTATION PROGRAM.");
+
+		// VARIABLES
+		double x1 = Math.random();
+		double y1 = Math.random();
+		double x2 = Math.random();
+		double y2 = Math.random();
+		float lineLength = (float) Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		System.out.println("Line length is " + lineLength + " units.");
+	}
+}
